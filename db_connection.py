6662ec503a08d70aeb047528327465d617d4bf20
syberkss <d.animashaun@outlook.com>
2022-12-08 10:26:29 +0000

    Updating database connection

---
+++ db_connection.py
@@ -0,0 +1,25 @@
+import psycopg2
+
+#Connecting to existing database
+conn = psycopg2.connect(
+    database="database",
+    user="docker",
+    password="docker",
+    host="0.0.0.0"
+)
+
+#Open cursor t perform database operations
+cur = conn.cursor()
+
+#query the database
+cur.execute("")
+rows = cur.fetchall()
+
+if not len(rows):
+    print("Empty")
+else:
+    for row in rows:
+        print(row)
+#Close communication with database
+cur.close()
+conn.close()
